7ab95afbd1c59b91397a1ab33d9a302c06d6067e
c-hennayake <info.chandana667@gmail.com>
2026-06-07 21:34:11 +0530

    Add repository and unit of work contracts

---
+++ TaskManager.Application/Abstractions/Persistence/ITaskRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Application.Abstractions.Persistence
+{
+    internal interface InterfITaskRepository
+    {
+    }
+}
---
+++ TaskManager.Domain/Entities/TaskItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Domain.Entities
+{
+    internal class TaskItem
+    {
+    }
+}
